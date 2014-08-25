5dcce127f98442b34c359586dc786804e155cdb2
Daniel Kertesz <sand@gagarin.(none)>
2014-08-25 17:07:10 +0200

    include "." in the characters allowed in the URL regexps

--- web.go
+++ web.go
@@ -214,11 +214,11 @@ func RunServer(address string, storage Storage) error {
 	r.Handle("/logout", WithRequest(ac, vHandlerFunc(Logout))).Name("logout")
 	r.Handle("/ls", WithRequest(ac, vHandlerFunc(ListPages))).Name("list_pages")
 
-	r.Handle("/{pagepath:[a-zA-Z0-9/]+}", WithRequest(ac, vHandlerFunc(EditPage))).Queries("edit", "1").Name("edit_page")
-	r.Handle("/{pagepath:[a-zA-Z0-9/]+}", WithRequest(ac, vHandlerFunc(ShowPageLog))).Queries("log", "1").Name("show_log")
-	r.Handle("/{pagepath:[a-zA-Z0-9/]+}", WithRequest(ac, vHandlerFunc(RenamePage))).Queries("rename", "1").Name("rename_page")
+	r.Handle("/{pagepath:[a-zA-Z0-9/.]+}", WithRequest(ac, vHandlerFunc(EditPage))).Queries("edit", "1").Name("edit_page")
+	r.Handle("/{pagepath:[a-zA-Z0-9/.]+}", WithRequest(ac, vHandlerFunc(ShowPageLog))).Queries("log", "1").Name("show_log")
+	r.Handle("/{pagepath:[a-zA-Z0-9/.]+}", WithRequest(ac, vHandlerFunc(RenamePage))).Queries("rename", "1").Name("rename_page")
 
-	r.Handle("/{pagepath:[a-zA-Z0-9/]+}", WithRequest(ac, vHandlerFunc(ShowPage))).Name("show_page")
+	r.Handle("/{pagepath:[a-zA-Z0-9/.]+}", WithRequest(ac, vHandlerFunc(ShowPage))).Name("show_page")
 	http.Handle("/", r)
 
 	log.Printf("Listening on: %s\n", address)
