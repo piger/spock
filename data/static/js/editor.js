9db1b5c205c9a02900120160aca39274b6a4d25e
Daniel Kertesz <daniel@spatof.org>
2014-08-29 23:13:55 +0200

    codemirror editor

---
+++ data/static/js/editor.js
@@ -0,0 +1,29 @@
+CodeMirror.defineMode("wikiEditor", function(config) {
+    var mode;
+    if (pageMode !== "") {
+        mode = pageMode;
+    } else {
+        mode = "markdown";
+    }
+
+    return CodeMirror.multiplexingMode(
+        CodeMirror.getMode(config, mode),
+        {open: "---\n", close: "---\n",
+         mode: CodeMirror.getMode(config, "yaml"),
+         delimStyle: "delimit"}
+    );
+});
+
+var editor = CodeMirror.fromTextArea(document.getElementById("content-area"), {
+    mode: "wikiEditor",
+    lineNumbers: true,
+    theme: "mdn-like",
+    styleActiveLine: true
+});
+
+$(document).ready(function() {
+    $("#cancel-btn").on('click', function () {
+        var u = window.location.href;
+        window.location.href = u.replace("?edit=1", "");
+    });
+});
