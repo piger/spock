8871012b0df2601269748c92f5d34bf6fd1be617
Daniel Kertesz <daniel@spatof.org>
2014-09-14 20:24:54 +0200

    add some more targets to the makefile

--- Makefile
+++ Makefile
@@ -1,4 +1,15 @@
-GO_BINDATA = go-bindata
+GO_BINDATA := go-bindata
+GO_FILES := $(shell find . -name '*.go')
 
-static-data.go: data
+all: spock
+
+spock: $(GO_FILES)
+	./build-osx.sh
+
+static-data.go:
 	$(GO_BINDATA) -o static-data.go -pkg spock -tags bundle -ignore '~\z' -prefix data/ data/...
+
+static-data.tar.gz:
+	tar zcvf static-data.tar.gz data/
+
+.PHONY: static-data.go static-data.tar.gz
--- build-osx.sh
+++ build-osx.sh
@@ -3,10 +3,9 @@
 
 set -e
 
-LIBICU=$(dirname $(brew list icu4c | fgrep readme.html))
-CGO_LDFLAGS="-L${LIBICU}/lib"
-CGO_CFLAGS="-I${LIBICU}/include"
-DYLD_LIBRARY_PATH="${LIBICU}/lib"
+libicu=$(dirname $(brew list icu4c | fgrep readme.html))
+CGO_LDFLAGS="-L${libicu}/lib"
+CGO_CFLAGS="-I${libicu}/include"
 export CGO_LDFLAGS CGO_CFLAGS
 
 go build -tags "libstemmer icu leveldb" cmd/spock/spock.go
